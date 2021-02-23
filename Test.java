8ea332fa85b6dc60334a85d68053343acc776b6f
Annette Pienaar <576370@student.belgiumcampus.ac.za>
2021-02-23 19:38:49 +0200

    Deleted test.java

--- Test.java
+++
@@ -1,9 +0,0 @@
-//I have an announcement... the test works
-import java.rmi.server.UID; //Generate random user ID
-public class Test{
-    public static void main(String[] args) {
-        UID userId = new UID();//Generate random user ID
-        System.out.println("userId: " + userId);//Generate random user ID
-    }
-    
-}
