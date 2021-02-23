e67bab5a6a079064bb4d2a06da1609378d25173f
Annette Pienaar <576370@student.belgiumcampus.ac.za>
2021-02-23 17:01:40 +0200

    Added some classes

--- Test.java
+++ Test.java
@@ -1,7 +1,9 @@
 //I have an announcement... the test works
-
+import java.rmi.server.UID; //Generate random user ID
 public class Test{
     public static void main(String[] args) {
-        
+        UID userId = new UID();//Generate random user ID
+        System.out.println("userId: " + userId);//Generate random user ID
     }
+    
 }
